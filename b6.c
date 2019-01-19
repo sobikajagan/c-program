025077ff858cf864cdd0cbd19a83b88b70a12b66
sobikajagan <46813549+sobikajagan@users.noreply.github.com>
2019-01-19 20:10:36 +0530

    Update b6.c

--- b6.c
+++ b6.c
@@ -6,7 +6,11 @@ int main()
     scanf("%d",&a);
     if(((a%4==0)&&(a%100!=0))||(a%400==0))
     {
-      printf("leap");
+      printf("yes");
+    }
+    else
+    {
+        printf("no");
     }
     return 0;
 }
