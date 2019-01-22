813b4704892dc7e1b3c6b4f58fc7c95a626a66e6
sobikajagan <46813549+sobikajagan@users.noreply.github.com>
2019-01-22 15:57:38 +0530

    Create h6.c

---
+++ h6.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void) {
+ int a[100],n,i,j;
+ scanf("%d",&n);
+ for (i=0;i<n;i++)
+ {
+   scanf("%d",&a[i]);
+ }
+ for (i=0;i<n;i++)
+ {
+   printf("%d\t",a[i]);
+ }
+ printf("\n");
+ for(i=0;i<n-1;i++)
+ {
+   for(j=i+1;j<=n;j++)
+   {
+     if(a[i]==a[j])
+     {
+     
+       printf("%d",a[i]);
+       goto k;
+     }
+       
+   }
+ }
+ k:
+  return 0;
+}
