3b149304945bc06a6e39eabdf92f8dd66ec6378e
sobikajagan <46813549+sobikajagan@users.noreply.github.com>
2019-01-22 19:49:03 +0530

    Create h4.c

---
+++ h4.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main() {
+  int a[100],n,i,j,c=0;
+  scanf("%d",&n);
+  for(i=0;i<n;i++)
+  {
+    scanf("%d",&a[i]);
+  }
+    for(i=0;i<n;i++)
+  {
+    printf("%d\t",a[i]);
+  }
+  printf("\n");
+  for(i=0;i<n;i++)
+  {
+    c=0;
+    for(j=0;j<n;j++)
+    {
+      if(a[i]==a[j])
+      {
+       c++;
+      }
+    }
+    if(c==1)
+    {
+      printf("%d\t",a[i]);
+    }
+  }
+  return 0;
+}
