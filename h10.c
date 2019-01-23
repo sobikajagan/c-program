3a1d2a655dd04ae93af595290a878898f863b33d
sobikajagan <46813549+sobikajagan@users.noreply.github.com>
2019-01-23 16:34:02 +0530

    Create h10.c

---
+++ h10.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main(void) {
+  int a[100],b[100],i,j,n,m,c=0,k;
+  scanf("%d\t%d",&n,&m);
+  for(i=0;i<n;i++)
+  {
+    scanf("%d",&a[i]);
+  }
+  for(i=0;i<m;i++)
+  {
+    scanf("%d",&b[i]);
+  }
+  for(i=0;i<n;i++)
+  {
+    for(j=0;j<m;j++)
+    {
+     if(a[i]==b[j])
+     {
+      c++;
+     }
+     
+     if(c==m)
+     {
+       printf("yes");
+       goto k;
+     }
+     else
+     {
+       printf("no");
+       goto k;
+     }
+    }
+
+  }
+
+ k:
+  return 0;
+}
