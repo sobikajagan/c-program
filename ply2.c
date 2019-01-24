22f3bf78f6a920f9094f1abe33cd9a2a2c9427f4
sobikajagan <46813549+sobikajagan@users.noreply.github.com>
2019-01-24 20:15:22 +0530

    Create ply2.c

---
+++ ply2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+  
+  int n,i,f=1;
+  scanf("%d",&n); 
+  for(i=1;i<=n;i++)
+  {
+    f=f*i;
+  }
+    printf("%d",f);
+    
+  
+     return 0;
+}
+
+
