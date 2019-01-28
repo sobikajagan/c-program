67d419e151aa098a03cbd050c28f6633953ace25
sobikajagan <46813549+sobikajagan@users.noreply.github.com>
2019-01-28 15:57:59 +0530

    Create b17.c

---
+++ b17.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include<math.h>
+
+int main(void) {
+  int n,s,r=0,rem;
+  scanf("%d",&n);
+  s=n;
+  while(n!=0)
+  {
+    rem=n%10;
+    r+=rem*rem*rem;
+    n=n/10;
+  }
+  if(s==r)
+  {
+    printf("armstrong");
+  }
+  else
+  {
+    printf("not armdtrong");
+  }
+  
+  return 0;
+}
