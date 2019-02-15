6b38ea21136ddbbd35cdd7cebf207aa4b6b1f6f2
sobikajagan <46813549+sobikajagan@users.noreply.github.com>
2019-02-15 19:13:19 +0530

    Create b102.c

---
+++ b102.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<string.h>
+
+int main(void) {
+ int i,n;
+ scanf("%d",&n);
+ i=n/2;
+ if(n%2==0)
+ {
+   
+   printf("%d",i);
+   
+ }
+ else
+ {
+   printf("%d",n);
+ }
+  return 0;
+}
