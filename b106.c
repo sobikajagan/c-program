16bc08db8e3258e731695feb5bd0fb3363b55616
sobikajagan <46813549+sobikajagan@users.noreply.github.com>
2019-02-18 19:04:35 +0530

    Create b106.c

---
+++ b106.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+  int n,i,k,a[10],b[10];
+  scanf("%d\t%d",&n,&k);
+  for(i=0;i<n;i++)
+  {
+    scanf("%d",&b[i]);
+  }
+  for(i=0;i<n;i++)
+  {
+    if(i%2!=0)
+    {
+    
+    }
+      
+  }
+  printf("%d\t",b[k]);
+
+  
+  return 0;
+}
