81b9e8e256e5f5e9b1fc24c668c66f1e743e1392
sobikajagan <46813549+sobikajagan@users.noreply.github.com>
2019-02-19 20:26:17 +0530

    Create bb23.c

---
+++ bb23.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+int a[20],n,i,j=0;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+ scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+ if(a[i]<j)
+ {
+  j=a[i];
+ }
+}
+printf("%d",j);
+  return 0;
+}
