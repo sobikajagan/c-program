a3359e2610fb1bf01dde77e20e839a0001f25f7a
sobikajagan <46813549+sobikajagan@users.noreply.github.com>
2019-02-20 21:52:25 +0530

    Create b41.c

---
+++ b41.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+  int n,i;
+  char a[10];
+  scanf("%s",a);
+  scanf("%d",&n);
+  for(i=0;i<n;i++)
+  {
+  printf("%s",a);
+  printf("\n");
+   }
+
+}
