bb31ce2cbfd0a77ca56e53146f23c815ceb44a43
sobikajagan <46813549+sobikajagan@users.noreply.github.com>
2019-02-23 20:12:45 +0530

    Create ply11.c

---
+++ ply11.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+char a[10];
+scanf("%s",&a);
+
+if(a[0]=='S' || a[0]=='s')
+{
+  printf("Yes");
+}
+else 
+{
+printf("No");
+}
+
+  return 0;
+}
