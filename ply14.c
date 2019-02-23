7b3fc5616a0482abc5568153e1d383db212c3e29
sobikajagan <46813549+sobikajagan@users.noreply.github.com>
2019-02-23 21:47:20 +0530

    Create ply14.c

---
+++ ply14.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) 
+{
+  char a[100],l,n,i;
+  scanf("%s",a);
+  n=strlen(a);
+  for(i=n-1;i>=0;i--)
+  {
+  if(a[i]=='a' || a[i]=='e' || a[i]=='i' || a[i]=='o' ||a[i]=='u')
+  {
+  }
+  else
+  {
+    printf("%c",a[i]);
+
+  }
+  }
+
+  return 0;
+}
