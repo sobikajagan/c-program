d350137af457f39f534b1daaf610ba5d9bad2dc0
sobikajagan <46813549+sobikajagan@users.noreply.github.com>
2019-02-24 20:43:45 +0530

    Create b78.c

---
+++ b78.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) 
+{
+  int n;
+  scanf("%d",&n);
+    if(n%13==0)
+    {
+      printf("yes");
+    }
+    else
+    {
+      printf("no");
+    }
+  
+  return 0;
+}
