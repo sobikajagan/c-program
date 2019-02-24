2baefeb3c8302e08a421ae3ac94dbe4de1bf192b
sobikajagan <46813549+sobikajagan@users.noreply.github.com>
2019-02-24 18:50:45 +0530

    Create b68.c

---
+++ b68.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) 
+{
+int n,i,j,f;
+scanf("%d",&n);
+if(n%7==0)
+{
+printf("yes");
+}
+else 
+{
+  printf("no");
+  
+}
+
+return 0;
+}
