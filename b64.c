0f4b55839132316788996417fa4a97451b442ae6
sobikajagan <46813549+sobikajagan@users.noreply.github.com>
2019-02-24 18:27:20 +0530

    Create b64.c

---
+++ b64.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) 
+{
+int a,b,c;
+scanf("%d\t%d",&a,&b);
+c=a+b;
+if(c%2==0)
+{
+  printf("even");
+}
+else 
+{
+  printf("odd");
+}
+
+	return 0;
+}
