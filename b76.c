375fbb4b5f33b265b23839ac94eeddc776248c12
sobikajagan <46813549+sobikajagan@users.noreply.github.com>
2019-02-24 20:30:51 +0530

    Create b76.c

---
+++ b76.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) 
+{
+  int n,i;
+  scanf("%d",&n);
+  for(i=1;i<=n;i++)
+  {
+    if(n%i==0)
+    {
+      printf("%d\t",i);
+    }
+  }
+  
+  return 0;
+}
