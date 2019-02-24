c200df848e3f1f50ffddfa88748c5ebc08ac1d06
sobikajagan <46813549+sobikajagan@users.noreply.github.com>
2019-02-24 13:00:06 +0530

    Create b27.c

---
+++ b27.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) 
+{
+  int n;
+   scanf("%d",&n);
+   if(n<0 || n>0)
+   {
+     printf("Yes");
+   }
+   else
+   {
+     printf("No");
+   }
+  
+     return 0;
+}
