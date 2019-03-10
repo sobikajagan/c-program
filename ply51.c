9e7f70ac1caeb48e2a50607a8230cf943d8ace8c
sobikajagan <46813549+sobikajagan@users.noreply.github.com>
2019-03-10 20:10:56 +0530

    Create ply51.c

---
+++ ply51.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+int main()
+{
+int a[10],n,i,j,t=0;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+  scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+  for(j=i+1;j<n;j++)
+  {
+  if(a[i]>a[j])
+  {
+    t=a[i];
+    a[i]=a[j];
+    a[j]=t;
+  }
+  }
+}
+printf("%d",a[1]);
+return 0;
+}
