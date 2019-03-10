6f6371b104a0419338f93e3107209779bfe2bc5d
sobikajagan <46813549+sobikajagan@users.noreply.github.com>
2019-03-10 20:26:48 +0530

    Create ply54.c

---
+++ ply54.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+int main()
+{
+char a[10],b[10];
+int i,j,l,m;
+scanf("%s\t%s",a,b);
+l=strlen(a);
+m=strlen(b);
+if( a[i]==b[i])
+{
+  printf("yes");
+}
+else{
+  printf("no");
+}
+return 0;
+}
