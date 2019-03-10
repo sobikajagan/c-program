561bac2b6447a6256d9f0098770162904d902cf6
sobikajagan <46813549+sobikajagan@users.noreply.github.com>
2019-03-10 10:29:24 +0530

    Create ply26.c

---
+++ ply26.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<string.h>
+
+int main(void) {
+ char a[100],b[100];
+ int n,i;
+ scanf("%[^\n]%*c",a);
+ n=strlen(a);
+ for(i=0;i<n;i++)
+ {
+   if(a[i]!=' '||a[i-1]==' ')
+   {
+     printf("%c",a[i]);
+   }
+  }
+  return 0;
+}
