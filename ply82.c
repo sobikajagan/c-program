125c679d24e26e6759b5636ca752552e7fda6cdb
sobikajagan <46813549+sobikajagan@users.noreply.github.com>
2019-03-12 06:56:40 +0530

    Rename ply2.c to ply82.c

---
+++ ply82.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+int a[10],n,i,j,s,max;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+  scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+  for(j=i+1;j<n;j++)
+  {
+  s=a[i]&a[j];
+  
+}
+}
+printf("%d",s);
+  return 0;
+}
