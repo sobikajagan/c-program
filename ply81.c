ca710a6690ff2d548d1733c0089c8e89cf54398b
sobikajagan <46813549+sobikajagan@users.noreply.github.com>
2019-03-12 07:02:08 +0530

    Create ply81.c

---
+++ ply81.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void) {
+int a[10],n,i,j,s,max;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+  scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+  for(j=i+1;j<n;j++)
+  {
+  s=a[i]&a[j];
+  if(s>max)
+  {
+    max=s;
+  }
+  
+}
+}
+printf("%d",max);
+  return 0;
+}
